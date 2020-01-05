70af05da7eeb0111f746e632a90582b1a1d1f8aa
EDownie <53910027+ED1978@users.noreply.github.com>
2020-01-05 21:43:10 +0000

    sell pet to customer all integration tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -78,8 +78,11 @@ def customer_can_afford_pet(customer, pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  add_pet_to_customer(customer, pet)
-  increase_pets_sold(pet_shop, 1)
-  remove_customer_cash(customer, pet[:price])
-  add_or_remove_cash(pet_shop, pet[:price])
+  pet_name = find_pet_by_name(pet_shop, pet)
+  if pet_name != pet && customer_can_afford_pet(customer, pet) == true
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop, pet[:price])
+  end
 end
